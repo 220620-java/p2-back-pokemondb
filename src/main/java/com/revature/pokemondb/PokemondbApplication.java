f62009024fe5b1a8ab0ac560f245e7f0e4fefa8a
Colby Tang <cobtang@gmail.com>
2022-07-26 09:36:19 -0500

    Added initial Spring files

---
+++ src/main/java/com/revature/pokemondb/PokemondbApplication.java
@@ -0,0 +1,13 @@
+package com.revature.pokemondb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemondbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemondbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/pokemondb/PokemondbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.pokemondb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemondbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
