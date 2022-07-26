e1f7735ed52933b8186c67a37360d3136c18091b
Dylan Cooley <78048848+dcee96@users.noreply.github.com>
2022-07-26 16:31:13 -0400

    Add files via upload

--- pokemon-DDL.sql
+++ pokemon-DDL.sql
@@ -1,53 +1,53 @@
 create schema pokemon_db;
 
 create table users (
-	id serial primary key,
+	id serial8 primary key,
 	username varchar(40) not null unique,
 	email varChar(100) not null unique,
 	is_admin boolean not null,
 	passwd varChar(20) not null,
 	salt bytea,
-	token_issued_at timestamp,
-	registered_at timestamp
+	token_issued_at timestamp default current_TIMESTAMP,
+	registered_at timestamp default current_TIMESTAMP
 );
 
 create table pokemon_comments (
-	id serial primary key,
+	id serial8 primary key,
 	pokemon_id int not null,
 	user_id int references users,
 	comment_content text,
 	is_flagged boolean,
 	likes int,
-	posted_at timestamp
+	posted_at timestamp default current_TIMESTAMP
 );
 
 create table pokemon_wishlists (
 	pokemon_id int references pokemon_comments,
 	user_id int references users,
-	created_at timestamp
+	created_at timestamp default current_TIMESTAMP
 );
 
 create table pokemon_fanart ( 
-	id serial primary key,
+	id serial8 primary key,
 	pokemon_id int references pokemon_comments,
 	user_id int references users,
 	image text,
 	is_flagged boolean,
-	uploaded_at timestamp
+	uploaded_at timestamp default current_TIMESTAMP
 );
 
 create table banned_users(
 	user_id int references users,
-	ban_duration timestamp,
+	ban_duration timestamp default current_TIMESTAMP,
 	ban_reason text,
-	banned_at timestamp
+	banned_at timestamp default current_TIMESTAMP
 );
 
 create table fanart_comments (
-	id serial primary key,
+	id serial8 primary key,
 	fanart_id int references pokemon_fanart,
 	user_id int references users,
 	comment_content text,
 	likes int,
-	posted_at timestamp
+	posted_at timestamp default current_TIMESTAMP
 );
