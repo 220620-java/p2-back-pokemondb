ec8fc0613ca777a1a067736d241f78276bfe5b46
Dylan Cooley <78048848+dcee96@users.noreply.github.com>
2022-07-26 16:46:04 -0400

    updated DDL

--- pokemon-DDL.sql
+++ pokemon-DDL.sql
@@ -1,4 +1,9 @@
-create schema pokemon_db;
+--create schema pokemon_db;
+
+create table pokemon ( 
+	id int primary key not null,
+	pokemon varchar not null
+);
 
 create table users (
 	id serial8 primary key,
@@ -13,8 +18,8 @@ create table users (
 
 create table pokemon_comments (
 	id serial8 primary key,
-	pokemon_id int not null,
-	user_id int references users,
+	pokemon_id int references pokemon  on delete cascade,
+	user_id int8 references users on delete cascade,
 	comment_content text,
 	is_flagged boolean,
 	likes int,
@@ -22,22 +27,22 @@ create table pokemon_comments (
 );
 
 create table pokemon_wishlists (
-	pokemon_id int references pokemon_comments,
-	user_id int references users,
+	pokemon_id int references pokemon on delete cascade,
+	user_id int8 references users on delete cascade,
 	created_at timestamp default current_TIMESTAMP
 );
 
 create table pokemon_fanart ( 
 	id serial8 primary key,
-	pokemon_id int references pokemon_comments,
-	user_id int references users,
+	pokemon_id int references pokemon on delete cascade,
+	user_id int8 references users on delete cascade,
 	image text,
 	is_flagged boolean,
 	uploaded_at timestamp default current_TIMESTAMP
 );
 
-create table banned_users(
-	user_id int references users,
+create table banned_users (
+	user_id int8 references users on delete cascade,
 	ban_duration timestamp default current_TIMESTAMP,
 	ban_reason text,
 	banned_at timestamp default current_TIMESTAMP
@@ -45,9 +50,11 @@ create table banned_users(
 
 create table fanart_comments (
 	id serial8 primary key,
-	fanart_id int references pokemon_fanart,
-	user_id int references users,
+	fanart_id int8 references pokemon_fanart on delete cascade,
+	user_id int8 references users on delete cascade,
 	comment_content text,
 	likes int,
 	posted_at timestamp default current_TIMESTAMP
 );
+
+
