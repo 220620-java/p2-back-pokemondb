c53fc80523a84d03bcd609164407a2be584c021a
Barry Norton <barrynorton107@gmail.com>
2022-07-29 00:13:32 -0400

    More work on fanart backend

--- src/main/java/com/revature/pokemondb/repositories/ArtCommRepository.java
+++ src/main/java/com/revature/pokemondb/repositories/ArtCommRepository.java
@@ -2,11 +2,18 @@ package com.revature.pokemondb.repositories;
 
 import org.springframework.stereotype.Repository;
 
-import com.revature.pokemondb.models.Fanart;
+import com.revature.pokemondb.models.ArtComment;
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * 
+ * @author Barry Norton
+ *
+ */
 @Repository
-public interface ArtCommRepository extends JpaRepository<Fanart, Integer>{
-
+public interface ArtCommRepository extends JpaRepository<ArtComment, Integer>{
+	
 }
