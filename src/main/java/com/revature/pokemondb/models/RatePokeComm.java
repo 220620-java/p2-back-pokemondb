d02f26b72a468e46216ebc5b787fa39d08d62e54
Barry Norton <barrynorton107@gmail.com>
2022-07-28 20:53:33 -0400

    Added a basic frame to all rate/report models

--- src/main/java/com/revature/pokemondb/models/RatePokeComm.java
+++ src/main/java/com/revature/pokemondb/models/RatePokeComm.java
@@ -1,5 +1,21 @@
 package com.revature.pokemondb.models;
 
-public class RatePokeComm {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="rate_poke_comm")
+public class RatePokeComm {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private int comment_id;
+	@Column(name="user_id")
+	private int author;
+	@Column(name="is_liked")
+	private Boolean isLiked;
 }
