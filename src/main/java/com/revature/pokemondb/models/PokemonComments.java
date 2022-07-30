827d754e83204aef996f2edb7a563a6f07716e77
coole <83263997+dcee-py@users.noreply.github.com>
2022-07-30 00:54:26 -0400

    adjusting model

--- src/main/java/com/revature/pokemondb/models/PokemonComments.java
+++ src/main/java/com/revature/pokemondb/models/PokemonComments.java
@@ -1,16 +1,11 @@
 package com.revature.pokemondb.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.time.Instant;
 import java.util.Objects;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="pokemon_comments", schema = "pokemon_db")
@@ -25,12 +20,16 @@ public class PokemonComments {
 	 private Pokemon pokemon;
 	 @ManyToOne
 	 @JoinColumn(name = "user_id", referencedColumnName="id")
-	 private User user;
+	 private user;
 	 private String comment_content;
 	 private Boolean is_flagged;
 	 private Integer likes;
 	 private Instant posted_at;
 
+
+	public PokemonComments() {
+
+	}
 	public PokemonComments(Long id, Pokemon pokemon, User user_id, String comment_content, Boolean is_flagged, Integer likes,
 			Instant posted_at) {
 		this.id = id;
@@ -41,8 +40,7 @@ public class PokemonComments {
 		this.likes = likes;
 		this.posted_at = posted_at;
 	}
-	
-	
+
 	public Long getId() {
 		return id;
 	}
--- src/main/java/com/revature/pokemondb/services/PokemonCommentService.java
+++ src/main/java/com/revature/pokemondb/services/PokemonCommentService.java
@@ -1,13 +1,12 @@
 package com.revature.pokemondb.services;
 
 import com.revature.pokemondb.models.PokemonComments;
-import com.revature.pokemondb.models.User;
 
 public interface PokemonCommentService {
 
     PokemonComments getAllComments();
 
-    PokemonComments storeNewComment(User user, PokemonComments comment);
+    PokemonComments storeNewComment( PokemonComments comment);
 
     PokemonComments deleteComment(PokemonComments comment);
 
