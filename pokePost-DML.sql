dee5a4be4c226940f0e31672c4b27a687c87981c
Barry Norton <barrynorton107@gmail.com>
2022-07-29 22:01:20 -0400

    changed db in order to connect

---
+++ pokePost-DML.sql
@@ -0,0 +1,5 @@
+-- users dml
+insert into users values (default, 'Barry912', 'barry912@revature.net', true, 'password', null, default, default);
+
+-- pokemon_fanart dml
+insert into pokemon_fanart values (default, null, 1, 'A Wild Pikachu', 'pikachu, barry912', 'images/sad_pikachu.png', 0, 0, default, default);
