b7b6a7c7ac65770b208220dc30f0bd4cbba2133d
zoomo <phengaroune.chan@gmail.com>
2022-08-03 14:35:18 -0400

    update

--- src/main/java/com/revature/pokemondb/controller/WishlistController.java
+++ src/main/java/com/revature/pokemondb/controller/WishlistController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.revature.pokemondb.models.Pokemon;
 import com.revature.pokemondb.models.Wishlist;
 import com.revature.pokemondb.services.PokemonService;
 import com.revature.pokemondb.services.WishlistService;
